c0d9f520d43b986c807b23a40be2855eaf596cea
manriquedv3452 <manriquedv@estudiantec.cr>
2018-05-06 22:42:23 -0600

    MAXIMIZE WITH OUT BIG M

--- main.c
+++ main.c
@@ -1,7 +1,57 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include "simplex.c"
+
+void print_matrix(double **matrix, int row_size, int column_size);
 
 int main(int argc, char* argv[])
 {
+	srand(time(NULL));
+	double **matrix = calloc(4, sizeof(double));
+	for (int i = 0; i < 4; i++)
+	{
+		matrix[i] = calloc(7, sizeof(double));
+	}
+	/*for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			matrix[i][j] = rand() % 10 + 1;
+		}
+	}*/
+
+	//fabrica de puertas y ventanas example test
+	matrix[0][0] = 1;
+	matrix[0][1] = -3;
+	matrix[0][2] = -5;
+	matrix[1][1] = 1;
+	matrix[1][3] = 1;
+	matrix[1][6] = 4;
+	matrix[2][2] = 2;
+	matrix[2][4] = 1;
+	matrix[2][6] = 12;
+	matrix[3][1] = 3;
+	matrix[3][2] = 2;
+	matrix[3][5] = 1;
+	matrix[3][6] = 18;
+	print_matrix(matrix, 4, 7);
+	printf("------------------------------------------\n");
+
+	maximize_algorithm(&matrix, 7, 4);
+
+	print_matrix(matrix, 4, 7);
 	return 0;
+}
+
+void print_matrix(double **matrix, int row_size, int column_size)
+{
+	for (int i = 0; i < row_size; i++)
+	{
+		for (int j = 0; j < column_size; j++)
+		{
+			printf("%.2f\t", matrix[i][j]);
+		}
+		printf("\n");
+	}
 }
--- matrix_operations.c
+++ matrix_operations.c
@@ -1,6 +1,6 @@
-void multiply_row_by_constant(int ***matrix, int row, int row_size, int constant);
-void add_multiplied_row_to_another(int ***matrix, int row_destiny, int row_to_add, 
-                                        int row_size, int constant);
+void multiply_row_by_constant(double ***matrix, int row, int row_size, double constant);
+void add_multiplied_row_to_another(double ***matrix, int row_destiny, int row_to_add, 
+                                        int row_size, double constant);
 
 /*Multiply a entire row by a constant
 Params:
@@ -12,7 +12,7 @@ Params:
 returns:
     void
 */
-void multiply_row_by_constant(int ***matrix, int row, int row_size, int constant)
+void multiply_row_by_constant(double ***matrix, int row, int row_size, double constant)
 {
     for (int i = 0; i < row_size; i++)
     {
@@ -27,13 +27,13 @@ Params:
     row_destiny = row to be added
     row_to_add = values of the row will be added 
     row_size = size of the row
-    constant = value that will multiply the values of row_to add
+    constant = value that will multiply the values of row_to_add
 
 returns:
     void
 */
-void add_multiplied_row_to_another(int ***matrix, int row_destiny, int row_to_add, 
-                                        int row_size, int constant)
+void add_multiplied_row_to_another(double ***matrix, int row_destiny, int row_to_add, 
+                                        int row_size, double constant)
 {
     //constant deafult 1;
 
